ba709433d96cfc88a0307576ba8a7e2b29703808
Tyler Farhner <tylerfarhner@gmail.com>
2021-02-05 10:40:13 -0800

    adds floating action button

--- src/screens/HomeListScreen.js
+++ src/screens/HomeListScreen.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { FloatingAction } from 'react-native-floating-action'
 
 import Card from '../components/Card'
 
 export default function HomeListScreen() {
     return (
-        <View>
+        <View style={ styles.container }>
             <Card />
+            <FloatingAction 
+                position="right"
+                animated={ false }
+                showBackground={ false }
+                onPressMain={ () => console.log('button is pressed') }
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    
+    container: {
+        flex: 1
+    }
     
 })
