52175e688c777a7bf7de82d9bea54fba6e4dcbaa
Tyler Farhner <tylerfarhner@gmail.com>
2021-02-05 12:51:56 -0800

    creates store

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
 
 import AppNavigator from './src/navigation/AppNavigator'
+import store from './src/redux/Store'
 
 export default function App() {
   return (
-    <AppNavigator />
+    <Provider
+      store={ store }
+    >
+      <AppNavigator />
+    </Provider>
   );
 }
 
