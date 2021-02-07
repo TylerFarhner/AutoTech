415c186626cea97752bf801f87970c6582bede5b
Tyler Farhner <tylerfarhner@gmail.com>
2021-02-06 19:13:28 -0800

    adds Formik, adds data to state

--- src/screens/AddHomeScreen.js
+++ src/screens/AddHomeScreen.js
@@ -1,62 +1,108 @@
 import React from 'react';
-import {StyleSheet, View, Text, ScrollView, TextInput, Button} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, TextInput, Button, KeyboardAvoidingView} from 'react-native';
+import { Formik } from 'formik'
 
 const AddHomeScreen = () => {
-    return (
-        <ScrollView>
-            <View style={styles.form}>
-                <View style={styles.formGroup}>
-                    <Text style={styles.label}>Title</Text>
-                    <TextInput 
-                        style={styles.input}
-                    />
-                </View>
-                <View style={styles.formGroup}>
-                    <Text style={styles.label}>Image URL</Text>
-                    <TextInput 
-                        style={styles.input}
-                    />
-                </View>
-                <View style={styles.formGroup}>
-                    <Text style={styles.label}>Home Type</Text>
-                    <TextInput 
-                        style={styles.input}
-                    />
-                </View>
-                <View style={styles.formGroup}>
-                    <Text style={styles.label}>Price</Text>
-                    <TextInput 
-                        style={styles.input}
-                    />
-                </View>
-                <View style={styles.formGroup}>
-                    <Text style={styles.label}>Year Built</Text>
-                    <TextInput 
-                        style={styles.input}
-                    />
-                </View>
-                <View style={styles.formGroup}>
-                    <Text style={styles.label}>Address</Text>
-                    <TextInput 
-                        style={styles.input}
-                        multiline
-                    />
-                </View>
-                <View style={styles.formGroup}>
-                    <Text style={styles.label}>Description</Text>
-                    <TextInput 
-                        style={styles.input}
-                        multiline
-                    />
-                </View>
 
-                <View style={styles.buttonContainer}>
-                    <Button 
-                        title="Add Home"
-                    />
-                </View>
-            </View>
-        </ScrollView>
+    
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.select({android: undefined, ios: 'padding'})}
+            keyboardVerticalOffset={100}
+            style={{ flex: 1 }}
+        >
+            <ScrollView>
+
+                <Formik
+                    initialValues={{
+                        title: '',
+                        image: '',
+                        homeType: '',
+                        price: '',
+                        yearBuilt: '',
+                        address: '',
+                        description: ''
+                    }}
+                    onSubmit={(values) => {
+                        console.log(values)
+                    }}
+                >
+                    {(props) => (
+                        <View style={styles.form}>
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Title</Text>
+                            <TextInput 
+                                style={styles.input}
+                                onChangeText={ props.handleChange('title') }
+                                value={ props.values.title }
+                            />
+                        </View>
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Image URL</Text>
+                            <TextInput 
+                                style={styles.input}
+                                onChangeText={ props.handleChange('image') }
+                                value={ props.values.image }
+                            />
+                        </View>
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Home Type</Text>
+                            <TextInput 
+                                style={styles.input}
+                                onChangeText={ props.handleChange('homeType') }
+                                value={ props.values.homeType }
+                            />
+                        </View>
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Price</Text>
+                            <TextInput 
+                                style={styles.input}
+                                keyboardType="numeric"
+                                onChangeText={ props.handleChange('price') }
+                                value={ props.values.price }
+                            />
+                        </View>
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Year Built</Text>
+                            <TextInput 
+                                style={styles.input}
+                                keyboardType="numeric"
+                                onChangeText={ props.handleChange('yearBuilt') }
+                                value={ props.values.yearBuilt }
+                            />
+                        </View>
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Address</Text>
+                            <TextInput 
+                                style={styles.input}
+                                multiline
+                                onChangeText={ props.handleChange('address') }
+                                value={ props.values.address }
+                            />
+                        </View>
+                        <View style={styles.formGroup}>
+                            <Text style={styles.label}>Description</Text>
+                            <TextInput 
+                                style={styles.input}
+                                multiline
+                                onChangeText={ props.handleChange('description') }
+                                value={ props.values.description }
+                            />
+                        </View>
+
+                        <View style={styles.buttonContainer}>
+                            <Button 
+                                title="Add Home"
+                                onPress={ props.handleSubmit }
+                            />
+                        </View>
+                    </View>
+                    )}
+                </Formik>
+
+
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
 
