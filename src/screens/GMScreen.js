4bc1208ffaa96e9bac7450a2f045ff23af5e5308
Tyler Farhner <tylerfarhner@gmail.com>
2021-09-04 16:00:35 -0700

    finishes building cards & organizes folders

--- src/screens/GMScreen.js
+++ src/screens/GMScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {StyleSheet, View, Text} from 'react-native'
+import { ScrollView } from "react-native-gesture-handler";
 
+// ------------- CARD IMPORTS -------------------------
 import EngineCard from "../components/Diagnostic Cards/EngineCard";
 import BrakeCard from "../components/Diagnostic Cards/BrakeCard";
-import { ScrollView } from "react-native-gesture-handler";
 
 const GMScreen = () => {
 
