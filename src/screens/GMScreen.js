3f3967cab79718854b77a3368c9a803d5f54b18b
Tyler Farhner <tylerfarhner@gmail.com>
2021-09-04 16:03:50 -0700

    imports all cards and renders

--- src/screens/GMScreen.js
+++ src/screens/GMScreen.js
@@ -5,6 +5,12 @@ import { ScrollView } from "react-native-gesture-handler";
 // ------------- CARD IMPORTS -------------------------
 import EngineCard from "../components/Diagnostic Cards/EngineCard";
 import BrakeCard from "../components/Diagnostic Cards/BrakeCard";
+import CoolingCard from "../components/Diagnostic Cards/CoolingCard"
+import WheelsTiresCard from "../components/Diagnostic Cards/WheelsTiresCard"
+import SuspensionCard from "../components/Diagnostic Cards/SuspensionCard"
+import ACCard from "../components/Diagnostic Cards/ACCard"
+import TransCard from "../components/Diagnostic Cards/TransCard"
+import DiffCard from "../components/Diagnostic Cards/DiffCard"
 
 const GMScreen = () => {
 
@@ -12,6 +18,12 @@ const GMScreen = () => {
         <ScrollView>
             <EngineCard />
             <BrakeCard />
+            <CoolingCard />
+            <WheelsTiresCard />
+            <SuspensionCard />
+            <ACCard />
+            <TransCard />
+            <DiffCard />
         </ScrollView>
     )
 
